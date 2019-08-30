982cf7f5e4ab94bccb4974cc2abc44f8278b0929
Artyom <53091678+artkrblv@users.noreply.github.com>
2019-08-30 20:01:44 +0300

    add and fix polyline and draggable balloons

--- src/AdressList/AddressList.js
+++ src/AdressList/AddressList.js
@@ -1,84 +1,83 @@
 import React from 'react';
 import Octicon, {X} from '@primer/octicons-react';
 import './AddressList.scss';
-import {Droppable, Draggable, DragDropContext} from "react-beautiful-dnd";
+import {Droppable, Draggable, DragDropContext} from 'react-beautiful-dnd';
 
 const reorder = (list, startIndex, endIndex) => {
-    const result = Array.from(list);
-    const [removed] = result.splice(startIndex, 1);
-    result.splice(endIndex, 0, removed);
+  const result = Array.from(list);
+  const [removed] = result.splice(startIndex, 1);
+  result.splice(endIndex, 0, removed);
 
-    return result;
+  return result;
 };
 
 class AddressList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.updateList = this.updateList.bind(this);
+  }
 
-    constructor(props) {
-        super(props);
-        this.updateList = this.updateList.bind(this);
+  updateList(result) {
+    let addresses = this.props.addressList;
+    let coordinates = this.props.coordinateList;
+
+    if (typeof result != 'number') { // при перетаскивании элемента result является объектом, при удалении - числом
+      if (!result.destination) {
+        return;
+      }
+      addresses = reorder(
+          addresses,
+          result.source.index,
+          result.destination.index
+      );
+      coordinates = reorder(
+          coordinates,
+          result.source.index,
+          result.destination.index
+      );
+    } else {
+      addresses.splice(result, 1);
+      coordinates.splice(result, 1);
     }
 
-    updateList(result) {
-        let addresses = this.props.addressList;
-        let coordinates = this.props.coordinateList;
+    this.props.updateParentStateCallback(addresses, coordinates);
+  }
 
-        if (typeof result != 'number') {  // при перетаскивании элемента result является объектом, при удалении - числом
-            if (!result.destination) {
-                return;
-            }
-            addresses = reorder(
-                addresses,
-                result.source.index,
-                result.destination.index
-            );
-            coordinates = reorder(
-                coordinates,
-                result.source.index,
-                result.destination.index
-            )
-        } else {
-            addresses.splice(result, 1);
-            coordinates.splice(result, 1);
-        }
-
-        this.props.updateParentStateCallback(addresses, coordinates);
-    }
-
-    render() {
-        return (
-            <DragDropContext onDragEnd={this.updateList}>
-                <Droppable droppableId="droppable">
-                    {(provided) => (
-                        <div
-                            {...provided.droppableProps}
-                            ref={provided.innerRef}
-                        >
-                            {this.props.addressList.map((address, index) => (
-                                <Draggable key={index} draggableId={index.toString()} index={index}>
-                                    {(provided) => (
-                                        <div
-                                            ref={provided.innerRef}
-                                            {...provided.draggableProps}
-                                            {...provided.dragHandleProps}
-                                        >
-                                            <li className="list-group-item">
-                                                <span className="address-text">{address}</span>
-                                                <a className="close-btn" href="#!"
-                                                   onClick={() => this.updateList(index)}>
-                                                    <Octicon icon={X}/>
-                                                </a>
-                                            </li>
-                                        </div>
-                                    )}
-                                </Draggable>
-                            ))}
-                            {provided.placeholder}
-                        </div>
-                    )}
-                </Droppable>
-            </DragDropContext>
-        )
-    }
+  render() {
+    return (
+      <DragDropContext onDragEnd={this.updateList}>
+        <Droppable droppableId="droppable">
+          {(provided) => (
+            <div
+              {...provided.droppableProps}
+              ref={provided.innerRef}
+            >
+              {this.props.addressList.map((address, index) => (
+                <Draggable key={index} draggableId={index.toString()} index={index}>
+                  {(provided) => (
+                    <div
+                      ref={provided.innerRef}
+                      {...provided.draggableProps}
+                      {...provided.dragHandleProps}
+                    >
+                      <li className="list-group-item">
+                        <span className="address-text">{address}</span>
+                        <a className="close-btn" href="#!"
+                          onClick={() => this.updateList(index)}>
+                          <Octicon icon={X}/>
+                        </a>
+                      </li>
+                    </div>
+                  )}
+                </Draggable>
+              ))}
+              {provided.placeholder}
+            </div>
+          )}
+        </Droppable>
+      </DragDropContext>
+    );
+  }
 }
 
 export default AddressList;
--- src/App.js
+++ src/App.js
@@ -5,117 +5,114 @@ import AddressList from './AdressList/AddressList';
 import Octicon, {Location} from '@primer/octicons-react';
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      coordinates: [],
+      addresses: [],
+    };
+    this.updateStateFromChild = this.updateStateFromChild.bind(this);
+  }
 
-    constructor() {
-        super();
-        this.state = {
-            coordinates: [],
-            addresses: [],
-        };
-        this.updateStateFromChild = this.updateStateFromChild.bind(this);
-    }
+  updateStateFromChild(addr, coord) {
+    this.setState({
+      addresses: addr,
+      coordinates: coord,
+    });
+  }
 
-    updateStateFromChild(addr, coord) {
-        this.setState({
-            addresses: addr,
-            coordinates: coord,
+  renameListItem(index, newCoordinate) {
+    const ymaps = window.ymaps;
+    ymaps.geocode(newCoordinate)
+        .then((resp) => {
+          const newName = resp.geoObjects.get(0).properties.getAll().text;
+          const coords = [...this.state.coordinates];
+          const addrs = [...this.state.addresses];
+          coords[index] = newCoordinate;
+          addrs[index] = newName;
+
+          this.setState({
+            coordinates: coords,
+            addresses: addrs,
+          });
         });
+  }
+
+  addPoint(address) {
+    const ymaps = window.ymaps;
+    ymaps.geocode(address)
+        .then((result) => {
+          const addedPoint = result.geoObjects.get(0).geometry.getCoordinates();
+          const points = [...this.state.coordinates];
+          points.push(addedPoint);
+
+          this.setState({coordinates: points});
+        });
+  }
+
+  _handleEnterDown(e) {
+    const addressValue = e.target.value;
+    if (e.key === 'Enter') {
+      if (addressValue) {
+        const addresses = this.state.addresses;
+        addresses.push(addressValue);
+        this.setState({addresses: addresses});
+
+        document.getElementById('addPoint').value = '';
+
+        this.addPoint(addressValue);
+      }
     }
+  }
 
-    renameListItem(index, newCoordinate) {
-        const ymaps = window.ymaps;
-        ymaps.geocode(newCoordinate)
-            .then(resp => {
-                const newName = resp.geoObjects.get(0).properties.getAll().text;
-                let coords = [...this.state.coordinates];
-                let addrs = [...this.state.addresses];
-                coords[index] = newCoordinate;
-                addrs[index] = newName;
+  render() {
+    return (
+      <div className="App">
 
-                this.setState({
-                    coordinates: coords,
-                    addresses: addrs
-                })
-            });
-
-    }
-
-    addPoint(address) {
-        const ymaps = window.ymaps;
-        ymaps.geocode(address)
-            .then((result) => {
-                const addedPoint = result.geoObjects.get(0).geometry.getCoordinates();
-                let points = [...this.state.coordinates];
-                points.push(addedPoint);
-
-                this.setState({coordinates: points});
-            })
-    }
-
-    _handleEnterDown(e) {
-        const addressValue = e.target.value;
-        if (e.key === 'Enter') {
-            if (addressValue) {
-                let addresses = this.state.addresses;
-                addresses.push(addressValue);
-                this.setState({addresses: addresses});
-
-                document.getElementById('addPoint').value = '';
-
-                this.addPoint(addressValue);
-            }
-        }
-    }
-
-    render() {
-        return (
-            <div className="App">
-
-                <div className="left-side">
-                    <div className="input-group mb-3">
-                        <div className="input-group-prepend">
-                            <Octicon icon={Location} id="location"/>
-                        </div>
-                        <input type="text" className="form-control" id="addPoint"
-                               placeholder="Точка назначения" onKeyDown={(e) => this._handleEnterDown(e)}/>
-                    </div>
-                    <ul className="list-group">
-                        <AddressList addressList={this.state.addresses}
-                                     coordinateList={this.state.coordinates}
-                                     updateParentStateCallback={this.updateStateFromChild}
-                        />
-                    </ul>
-                </div>
-
-                <div className="right-side">
-                    <YMaps>
-                        <Map defaultState={{center: [55.753215, 37.622504], zoom: 12}} width='100%' height='100%'
-                             id='map'>
-
-                            <ZoomControl options={{float: 'right'}}/>
-                            {this.state.coordinates.map((coordinate, index) =>
-                                <Placemark key={index}
-                                           onDragEnd={(e) => this.renameListItem(index, e.get('target').geometry.getCoordinates())}
-                                           geometry={coordinate}
-                                           options={{draggable: true}}
-                                           properties={{iconContent: index + 1}}
-                                />
-                            )}
-                            <Polyline geometry={[...this.state.coordinates]}
-                                      options={{
-                                          balloonCloseButton: false,
-                                          strokeColor: '#000',
-                                          strokeWidth: 4,
-                                          strokeOpacity: 0.5,
-                                      }}/>
-                        </Map>
-                    </YMaps>
-                </div>
+        <div className="left-side">
+          <div className="input-group mb-3">
+            <div className="input-group-prepend">
+              <Octicon icon={Location} id="location"/>
             </div>
-        );
-    }
+            <input type="text" className="form-control" id="addPoint"
+              placeholder="Точка назначения" onKeyDown={(e) => this._handleEnterDown(e)}/>
+          </div>
+          <ul className="list-group">
+            <AddressList addressList={this.state.addresses}
+              coordinateList={this.state.coordinates}
+              updateParentStateCallback={this.updateStateFromChild}
+            />
+          </ul>
+        </div>
 
+        <div className="right-side">
+          <YMaps>
+            <Map defaultState={{center: [55.753215, 37.622504], zoom: 12}} width='100%' height='100%'
+              id='map'>
 
+              <ZoomControl options={{float: 'right'}}/>
+              {this.state.coordinates.map((coordinate, index) =>
+                <Placemark key={index}
+                  onDragEnd={(e) =>
+                    this.renameListItem(index, e.get('target').geometry.getCoordinates())}
+                  geometry={coordinate}
+                  options={{draggable: true}}
+                  properties={{iconContent: index + 1}}
+                />
+              )}
+              <Polyline geometry={[...this.state.coordinates]}
+                options={{
+                  balloonCloseButton: false,
+                  strokeColor: '#000',
+                  strokeWidth: 4,
+                  strokeOpacity: 0.5,
+                }}/>
+            </Map>
+          </YMaps>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
